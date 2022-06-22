5f1b0ee309c62581ed2f14b7286af2ad9cbc4908
Marko Intihar <37149137+inti87@users.noreply.github.com>
2022-06-22 10:23:03 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const fName = "Angela";
+const lName = "Yu";
+const num = 7;
+
+ReactDOM.render(
+  <div>
+    <h1>Hello {`${fName} ${lName}`}!</h1>
+    <p>Your lucky number is {Math.floor(Math.random() * 10)}</p>
+  </div>,
+  document.getElementById("root")
+);
